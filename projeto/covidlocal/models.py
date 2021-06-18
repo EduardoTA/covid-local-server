9473b5b74df95ac10c1b91fbe07b96b44f91f658
EduardoTA <EduardoTA@live.com>
2021-06-18 14:30:57 -0300

    Model do paciente

--- projeto/covidlocal/models.py
+++ projeto/covidlocal/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Paciente(models.Model):
+    CNS = models.IntegerField()
+    CPF = models.IntegerField()
+    nome = models.CharField(max_length=100)
+    nomeMae = models.CharField(max_length=100)
+    nomeSocial = models.CharField(max_length=100)
+    dataNascimento = models.IntegerField()
+    sexo = models.TextChoices('Feminino', 'Masculino', 'Ignorado')
+    raca = models.TextChoices('AMARELA', 'BRANCA', 'INDIGENA', 'NAO INFORMADA', 'PARDA', 'PRETA')
+    telefone = models.IntegerField()
+    gestante = models.BooleanField()
+    puerpera = models.BooleanField()
+    pais = models.CharField(max_length=100)
+    UF = models.CharField(max_length=2)
+    municipio = models.CharField(max_length=100)
+    zona = models.TextChoices('Rural', 'Urbana')
+    logradouro = models.CharField(max_length=100)
+    numero = models.IntegerField()
+    bairro = models.CharField(max_length=100)
+    complemento = models.CharField(max_length=10)
+    email = models.CharField(max_length=100)
