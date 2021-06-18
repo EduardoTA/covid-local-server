e5d4673c41335cc32debb9f75cffa34c1cb1fb4a
EduardoTA <EduardoTA@live.com>
2021-06-18 16:04:24 -0300

    Forms Paciente

--- projeto/covidlocal/forms.py
+++ projeto/covidlocal/forms.py
@@ -6,5 +6,25 @@ class PacienteForm(forms.ModelForm):
     class Meta:
         model = Paciente
         fields = [
-            'nome'
+            'id',
+            'id_cand',
+            'isCNS',
+            'nome',
+            'nomeMae',
+            'nomeSocial',
+            'dataNascimento',
+            'sexo',
+            'raca',
+            'telefone',
+            'gestante',
+            'puerpera',
+            'pais',
+            'UF',
+            'municipio',
+            'zona',
+            'logradouro',
+            'numero',
+            'bairro',
+            'complemento',
+            'email'
         ]
