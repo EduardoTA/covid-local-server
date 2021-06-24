bda6484f6a3ff8da457fa6ccf49e56d817b76b76
Lucas <lucasnego0142@gmail.com>
2021-06-24 11:25:47 -0300

    autenticacao

--- projeto/covidlocal/views.py
+++ projeto/covidlocal/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required,user_passes_test
 from .models import Paciente
 from .forms import PacienteForm
 from .tasks import *
@@ -7,10 +10,12 @@ from .tasks import *
 from django_q.tasks import async_task
 from django.contrib import messages
 
+@login_required
 def cadastro_vacina(request):
     async_task("covidlocal.tasks.sincronizar")
     return render(request, "cadastro_vacina.html", {})
 
+@login_required
 def cadastro_paciente(request):
     form = PacienteForm()
     if request.method == "POST":
@@ -29,3 +34,18 @@ def cadastro_paciente(request):
 
 def menu_inicial(request):
     return render(request, "menu_inicial.html", {})
+
+@user_passes_test(lambda u: u.is_superuser)
+def cadastrar_usuario(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('')
+    else:
+        form = UserCreationForm()
+    return render(request, 'cadastrar_usuario.html', {'form': form})
